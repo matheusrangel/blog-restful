d1b1ad81a0d5eb37832617eabf971bc857d1c141
Matheus Rangel <matheusulysses159@gmail.com>
2016-03-27 19:46:49 -0300

    v1.0

---
+++ src/java/br/ifpb/pd/model/Noticia.java
@@ -0,0 +1,130 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ifpb.pd.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement(name = "noticia")
+public class Noticia implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String titulo;
+    
+    private String autor;
+    
+    private String conteudo;
+    
+    private Date data;
+    
+    public Noticia() {}
+
+    public Noticia(Long id, String titulo, String autor, String conteudo, Date data) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.conteudo = conteudo;
+        this.data = data;
+    }
+
+    public Long  getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.id);
+        hash = 59 * hash + Objects.hashCode(this.titulo);
+        hash = 59 * hash + Objects.hashCode(this.autor);
+        hash = 59 * hash + Objects.hashCode(this.conteudo);
+        hash = 59 * hash + Objects.hashCode(this.data);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Noticia other = (Noticia) obj;
+        if (!Objects.equals(this.titulo, other.titulo)) {
+            return false;
+        }
+        if (!Objects.equals(this.autor, other.autor)) {
+            return false;
+        }
+        if (!Objects.equals(this.conteudo, other.conteudo)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.data, other.data)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Noticia{" + "id=" + id + ", titulo=" + titulo + ", autor=" + autor + ", conteudo=" + conteudo + ", data=" + data + '}';
+    }
+    
+}
---
+++ src/java/br/ifpb/pd/server/BlogServer.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ifpb.pd.server;
+
+import br.ifpb.pd.model.Noticia;
+import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ */
+@Path("blog")
+public class BlogServer {
+
+    @Context
+    private UriInfo context;
+    
+    private EntityManager em;
+
+    public BlogServer() {
+        em = Persistence.createEntityManagerFactory("BlogPU").createEntityManager();
+    }
+    
+    @POST
+    @Consumes("application/xml")
+    public Response criarNoticia(Noticia noticia) {
+        Noticia n = em.find(Noticia.class, noticia.getId());
+        if (n == null) {
+            em.getTransaction().begin();
+            em.persist(noticia);
+            em.getTransaction().commit();
+            return Response.status(Response.Status.CREATED).build();
+        } else {
+            return Response.status(Response.Status.CONFLICT).build();
+        }
+    }
+    
+    @PUT
+    @Path("{id}/{titulo}")
+    @Produces("application/json")
+    public Response atualizarNoticia(@PathParam("id") Long id, @PathParam("titulo") String titulo) {
+        Noticia n = em.find(Noticia.class, id);
+        if (n != null) {
+            n.setTitulo(titulo);
+            em.getTransaction().begin();
+            em.merge(n);
+            em.getTransaction().commit();
+            return Response.ok(n, MediaType.APPLICATION_JSON).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+    
+    @DELETE
+    @Path("{id}")
+    @Produces("application/xml")
+    public Response removerNoticia(@PathParam("id") Long id) {
+        Noticia n = em.find(Noticia.class, id);
+        if (n!= null) {
+            em.getTransaction().begin();
+            em.remove(n);
+            em.getTransaction().commit();
+            return Response.ok(n, MediaType.APPLICATION_XML).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+    
+    @GET
+    @Produces("text/html")
+    public Response getNoticia(@QueryParam("id") Long id) {
+        Noticia n = em.find(Noticia.class, id);
+        if (n != null) {
+            String html = "<html><body>"
+                    + "id="+n.getId()
+                    +"<br>titulo="+n.getTitulo()
+                    +"<br>autor="+n.getAutor()
+                    +"<br>conteudo="+n.getConteudo()
+                    +"<br>data="+n.getData()
+                    +"</body></html>";
+            return Response.ok(html, MediaType.TEXT_HTML).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+}
